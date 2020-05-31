d07ac2dbae1d467e6d6d9f7ee776c1ddc7c6cf9a
atimchenko <taa@nic.ru>
2020-05-31 18:09:51 +0300

    Lesson_9

---
+++ brain_games/games/is_prime.py
@@ -0,0 +1,19 @@
+from brain_games.cli import answer
+from brain_games.cli import randomint
+from sympy import isprime
+
+
+def is_prime():
+    random = randomint()
+    print('Question: ' + str(random))
+    quest_answer = answer()
+    final = []
+    if isprime(random) is True and quest_answer == 'yes':
+        return True
+    else:
+        final.append(quest_answer)
+        if isprime(random) is True:
+            final.append('yes')
+        else:
+            final.append('no')
+        return final
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,24 +3,26 @@ from brain_games.games.brain_calc import brain_calc
 from brain_games.games.brain_even import even_game
 from brain_games.games.brain_gcd import brain_gcd
 from brain_games.games.brain_progressive import progressive
+from brain_games.games.is_prime import is_prime
 
 
 def round_calc():
     greet()
     print('What is the result of the expression?')
     name = welcome_user()
+    rounds = 3
     attempt = 0
-    while attempt < 3:
+    while attempt < rounds:
         result = brain_calc()
         if result is True:
             print('Correct!')
-            attempt = attempt + 1
+            attempt += 1
         else:
             print(str(result[0]) + " is wrong answer ;(."
                   "Correct answer was " + str(result[1]) + ".")
             print("Let's try again," + name)
             break
-    if attempt == 3:
+    if attempt == rounds:
         print('Congratulations, ' + name)
 
 
@@ -28,18 +30,19 @@ def round_even():
     greet()
     print('Answer "yes" if number even otherwise answer "no"')
     name = welcome_user()
+    rounds = 3
     attempt = 0
-    while attempt < 3:
+    while attempt < rounds:
         result = even_game()
         if result is True:
             print('Correct!')
-            attempt = attempt + 1
+            attempt += 1
         else:
             print(str(result[0]) + " is wrong answer ;(."
                   "Correct answer was " + str(result[1]) + ".")
             print("Let's try again," + name)
             break
-    if attempt == 3:
+    if attempt == rounds:
         print('Congratulations, ' + name)
 
 
@@ -47,18 +50,19 @@ def round_gcd():
     greet()
     print('Find the greatest common divisor of given numbers.')
     name = welcome_user()
+    rounds = 3
     attempt = 0
-    while attempt < 3:
+    while attempt < rounds:
         result = brain_gcd()
         if result is True:
             print('Correct!')
-            attempt = attempt + 1
+            attempt += 1
         else:
             print(str(result[0]) + " is wrong answer ;(."
                   "Correct answer was " + str(result[1]) + ".")
             print("Let's try again," + name)
             break
-    if attempt == 3:
+    if attempt == rounds:
         print('Congratulations, ' + name)
 
 
@@ -66,18 +70,39 @@ def round_progressive():
     greet()
     print('What number is missing in the progression?')
     name = welcome_user()
+    rounds = 3
     attempt = 0
-    while attempt < 3:
+    while attempt < rounds:
         result = progressive()
         if result is True:
             print('Correct!')
-            attempt = attempt + 1
+            attempt += 1
         else:
             print(str(result[0]) + " is wrong answer ;(."
                   "Correct answer was " + str(result[1]) + ".")
             print("Let's try again," + name)
             break
-    if attempt == 3:
+    if attempt == rounds:
+        print('Congratulations, ' + name)
+
+
+def round_isprime():
+    greet()
+    print('Answer "yes" if given number is prime. Otherwise answer "no"')
+    name = welcome_user()
+    rounds = 3
+    attempt = 0
+    while attempt < rounds:
+        result = is_prime()
+        if result is True:
+            print('Correct!')
+            attempt += 1
+        else:
+            print(str(result[0]) + " is wrong answer ;(."
+                  "Correct answer was " + str(result[1]) + ".")
+            print("Let's try again," + name)
+            break
+    if attempt == rounds:
         print('Congratulations, ' + name)
 
 
@@ -95,6 +120,10 @@ def gcd_game():
 
 def progressive_game():
     round_progressive()
+
+
+def isprime_game():
+    round_isprime()
 
 
 if __name__ == '__main__':
