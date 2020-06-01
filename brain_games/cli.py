9660fb3c12201e0570593a31278f84c6333ddb30
atimchenko <taa@nic.ru>
2020-06-01 17:50:29 +0300

    fixed engine game

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -44,3 +44,18 @@ def is_number(str):
         return True
     except ValueError:
         return False
+
+
+def game_instruction(game_name):
+    if game_name.__name__ == 'is_prime':
+        print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    elif game_name.__name__ == 'progressive':
+        print('What number is missing in the progression?')
+    elif game_name.__name__ == 'brain_gcd':
+        print('Find the greatest common divisor of given numbers.')
+    elif game_name.__name__ == 'even_game':
+        print('Answer "yes" if number even otherwise answer "no"')
+    elif game_name.__name__ == 'brain_calc':
+        print('What is the result of the expression?')
+    else:
+        print('i dont know this game')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,4 @@
-from brain_games.cli import welcome_user, greet
+from brain_games.cli import welcome_user, greet, game_instruction
 from brain_games.games.brain_calc import brain_calc
 from brain_games.games.brain_even import even_game
 from brain_games.games.brain_gcd import brain_gcd
@@ -6,94 +6,14 @@ from brain_games.games.brain_progressive import progressive
 from brain_games.games.is_prime import is_prime
 
 
-def round_calc():
+def game_engine(game):
     greet()
-    print('What is the result of the expression?')
+    game_instruction(game)
     name = welcome_user()
     rounds = 3
     attempt = 0
     while attempt < rounds:
-        result = brain_calc()
-        if result is True:
-            print('Correct!')
-            attempt += 1
-        else:
-            print(str(result[0]) + " is wrong answer ;(."
-                  "Correct answer was " + str(result[1]) + ".")
-            print("Let's try again," + name)
-            break
-    if attempt == rounds:
-        print('Congratulations, ' + name)
-
-
-def round_even():
-    greet()
-    print('Answer "yes" if number even otherwise answer "no"')
-    name = welcome_user()
-    rounds = 3
-    attempt = 0
-    while attempt < rounds:
-        result = even_game()
-        if result is True:
-            print('Correct!')
-            attempt += 1
-        else:
-            print(str(result[0]) + " is wrong answer ;(."
-                  "Correct answer was " + str(result[1]) + ".")
-            print("Let's try again," + name)
-            break
-    if attempt == rounds:
-        print('Congratulations, ' + name)
-
-
-def round_gcd():
-    greet()
-    print('Find the greatest common divisor of given numbers.')
-    name = welcome_user()
-    rounds = 3
-    attempt = 0
-    while attempt < rounds:
-        result = brain_gcd()
-        if result is True:
-            print('Correct!')
-            attempt += 1
-        else:
-            print(str(result[0]) + " is wrong answer ;(."
-                  "Correct answer was " + str(result[1]) + ".")
-            print("Let's try again," + name)
-            break
-    if attempt == rounds:
-        print('Congratulations, ' + name)
-
-
-def round_progressive():
-    greet()
-    print('What number is missing in the progression?')
-    name = welcome_user()
-    rounds = 3
-    attempt = 0
-    while attempt < rounds:
-        result = progressive()
-        if result is True:
-            print('Correct!')
-            attempt += 1
-        else:
-            print(str(result[0]) + " is wrong answer ;(."
-                  "Correct answer was " + str(result[1]) + ".")
-            print("Let's try again," + name)
-            break
-    if attempt == rounds:
-        print('Congratulations, ' + name)
-
-
-def round_isprime():
-    greet()
-    print('Answer "yes" if given number is prime. Otherwise answer "no"')
-    name = welcome_user()
-    rounds = 3
-    attempt = 0
-    while attempt < rounds:
-        result = is_prime()
+        result = game()
         if result is True:
             print('Correct!')
             attempt += 1
@@ -107,23 +27,23 @@ def round_isprime():
 
 
 def main():
-    round_calc()
+    game_engine(brain_calc)
 
 
 def sec():
-    round_even()
+    game_engine(even_game)
 
 
 def gcd_game():
-    round_gcd()
+    game_engine(brain_gcd)
 
 
 def progressive_game():
-    round_progressive()
+    game_engine(progressive)
 
 
 def isprime_game():
-    round_isprime()
+    game_engine(is_prime)
 
 
 if __name__ == '__main__':
