d2c569479a5e03d025b27d55dd7e1db5710a3973
atimchenko <taa@nic.ru>
2020-06-04 12:40:43 +0300

    fix even_game and cache

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -3,24 +3,22 @@ from brain_games.cli import randomint
 
 
 def even_game():
-    result = 0
-    while result < 3:
-        random = randomint()
-        print('Question: ' + str(random))
-        quest_answer = answer()
-        final = []
-        if random % 2 == 0 and \
-            quest_answer == 'yes' \
-            or random % 2 == 1 and \
-                quest_answer == 'no':
-            return True
+    random = randomint()
+    print('Question: ' + str(random))
+    quest_answer = answer()
+    final = []
+    if random % 2 == 0 and \
+        quest_answer == 'yes' \
+        or random % 2 == 1 and \
+            quest_answer == 'no':
+        return True
+    else:
+        final.append(quest_answer)
+        if random % 2 == 1:
+            final.append('no')
         else:
-            final.append(quest_answer)
-            if random % 2 == 1:
-                final.append('no')
-            else:
-                final.append('yes')
-            return final
+            final.append('yes')
+        return final
 
 
 def main():
